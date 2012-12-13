5a4169d6ee9ae2f27b2d388ec57d7a02e4a61c95
Vangelis Bibakis <bibakisv@gmail.com>
2012-12-13 11:45:18 +0200

    Minor documentation corrections

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 DemianFlexible
 ==============
 
-The Demian Flexible responsive Wordpress theme
+The Demian Flexible responsive Wordpress theme.
+
+A simple single column, stylish wordpress theme.
+
+Find out more about our products and developer resources at [Demian Labs](http://demianlabs.com)
--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
         <div class="footer-container">
             <footer class="wrapper">
-            	Theme <a href="http://demianlabs.com">Demian Labs</a> by <a href="http://demianlabs.com">Demian Labs</a>
+            	Theme <a href="https://github.com/DemianLabs/DemianFlexible">Demian Flexible</a> by <a href="http://demianlabs.com">Demian Labs</a>
             </footer>
         </div>
     </body>
